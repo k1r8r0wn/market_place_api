4a54877d7f0f8eec61f2b0c3f2a7607b1329fea8
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-28 17:17:18 +0600

    Add the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
                   constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1 do
       # resources
-      resources :users, only: [:show]
+      resources :users, only: [:show, :create]
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,42 @@ describe Api::V1::UsersController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+
+    context 'when is successfully created' do
+      before(:each) do
+        @user_attributes = attributes_for(:user)
+        post :create, params: { user: @user_attributes }, format: :json
+      end
+
+      it 'renders the json representation for the user record just created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when is not created' do
+      before(:each) do
+        # Notice: not including the email here
+        @invalid_user_attributes = { password: 'password',
+                                     password_confirmation: 'password' }
+        post :create, params: { user: @invalid_user_attributes }, format: :json
+      end
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on why the user could not be created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
