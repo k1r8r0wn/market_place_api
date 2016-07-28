44360286667ad888410c630fb05afbdef3abbcd3
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-28 12:11:17 +0600

    Add Api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json }, 
                   constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+      # resources
+    end
   end
 end
