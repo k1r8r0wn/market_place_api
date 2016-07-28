6af1a7ce949ad3ad1b297dfc89397fb36e342fcc
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-28 12:07:17 +0600

    Add routes with a namespace, default format and subdomain defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :api, defaults: { format: :json }, 
+                  constraints: { subdomain: 'api' }, path: '/' do
+  end
 end
