c094674821d0f1c40630541344203d32343b5c6f
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-28 12:53:54 +0600

    Add testing instruments, configure rspec

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,16 @@ module MarketPlaceApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
   end
 end
