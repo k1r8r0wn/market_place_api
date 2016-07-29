6c552cf4b82cac5769759f8925d18c5d4cdb6c51
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-29 11:57:14 +0600

    Refactor test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, type: :controller do
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
-  before(:each) { request.headers['Accept'] = Mime[:json] }
-  before(:each) { request.headers['Content-Type'] = Mime[:json].to_s }
-
   describe 'GET #show' do
     before(:each) do
       @user = create(:user) 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,11 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers
   #Including to test requests
   config.include Request::JsonHelpers, type: :controller
-  
+  config.include Request::HeadersHelpers, type: :controller
+
+  config.before(:each) do
+    include_default_accept_headers 
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
