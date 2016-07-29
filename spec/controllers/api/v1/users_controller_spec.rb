0d21b3734efb8eef72aaa0cf887bcc40cedcc68b
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-29 11:47:11 +0600

    Refactor json parse method

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,8 +10,7 @@ describe Api::V1::UsersController, type: :controller do
     end
 
     it 'returns the information about a reporter on a hash' do
-      user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email]).to eql @user.email
+      expect(json_response[:email]).to eql @user.email
     end
 
     it { should respond_with 200 }
@@ -26,8 +25,7 @@ describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders the json representation for the user record just created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql @user_attributes[:email]
+        expect(json_response[:email]).to eql @user_attributes[:email]
       end
 
       it { should respond_with 201 }
@@ -42,13 +40,11 @@ describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders an errors json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_response).to have_key(:errors)
       end
 
       it 'renders the json errors on why the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:errors][:email]).to include "can't be blank"
+        expect(json_response[:errors][:email]).to include "can't be blank"
       end
 
       it { should respond_with 422 }
@@ -68,8 +64,7 @@ describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders the json representation for the updated user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql 'newmail@example.com'
+        expect(json_response[:email]).to eql 'newmail@example.com'
       end
 
       it { should respond_with 200 }
@@ -82,13 +77,11 @@ describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders an errors json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_response).to have_key(:errors)
       end
 
       it 'renders the json errors on whye the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:errors][:email]).to include 'is invalid'
+        expect(json_response[:errors][:email]).to include 'is invalid'
       end
 
       it { should respond_with 422 }
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'devise'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -32,7 +32,9 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers
-
+  #Including to test requests
+  config.include Request::JsonHelpers, type: :controller
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
