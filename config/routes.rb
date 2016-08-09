e62af2ce8529fd7bcd24905b4e798c669d47b68a
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-08-09 16:03:10 +0600

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' }, path: '/' do
-    namespace :v1 do
+    namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # resources
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
