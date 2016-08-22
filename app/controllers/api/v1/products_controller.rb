19ad3f4ad31092f16c2bef3ffef069a58d428983
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-08-22 23:10:24 +0600

    Add more public product endpoints(create, update, destroy)

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,12 +1,46 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :authenticate_with_token!, only: [:create, :update]
+  before_action :find_product, only: [:update, :destroy]
   respond_to :json
-
+  
   def index
     respond_with Product.all
-    # p @response
   end
 
   def show
     respond_with Product.find(params[:id])
+  end
+  
+  def create
+    @product = current_user.products.build(product_params)
+
+    if @product.save
+      render json: @product, status: :created, location: [:api, :v1, @product]
+    else
+      render json: { errors: @product.errors }, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if @product.update(product_params)
+      render json: @product, status: :ok, location: [:api, :v1, @product]
+    else
+      render json: { errors: @product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
+  end
+
+  def find_product
+    @product = current_user.products.find(params[:id])
   end
 end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe Api::V1::ProductsController, type: :controller do
-  let(:product) { create(:product) }
+  let(:user)    { create(:user) }
+  let(:product) { create(:product, user: user) }
 
   describe "GET #index" do
     before(:each) do
@@ -26,5 +27,84 @@ describe Api::V1::ProductsController, type: :controller do
     end
 
     it { should respond_with 200 }
+  end
+  
+  describe 'POST #create' do
+    context 'when is successfully created' do
+      before(:each) do
+        @product_attributes = attributes_for(:product)
+        set_api_authorization_header user.auth_token
+        post :create, params: { user_id: user.id, product: @product_attributes }
+      end
+
+      it 'renders the json representation for the product record just created' do
+        expect(json_response[:title]).to eql @product_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when is not created' do
+      before(:each) do
+        @invalid_product_attributes = { title: 'Smart TV', price: 'Twelve dollars' }
+        set_api_authorization_header user.auth_token
+        post :create, params: { user_id: user.id, product: @invalid_product_attributes }
+      end
+
+      it 'renders an errors json' do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on whye the user could not be created' do
+        expect(json_response[:errors][:price]).to include 'is not a number'
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+  
+  describe 'PUT/PATCH #update' do
+    before(:each) do
+      set_api_authorization_header user.auth_token
+    end
+    
+    context 'when is successfully updated' do
+      before(:each) do
+        patch :update, params: { user_id: user.id, id: product.id,
+                                 product: { title: 'An expensive TV' } }
+      end
+
+      it 'renders the json representation for the updated [profuct]' do
+        expect(json_response[:title]).to eql 'An expensive TV'
+      end
+
+      it { should respond_with 200 }
+    end
+    
+    context 'when is not updated' do
+      before(:each) do
+        patch :update, params: { user_id: user.id, id: product.id,
+                                 product: { price: 'two hundred' } }
+      end
+
+      it 'renders an errors json' do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on whye the user could not be created' do
+        expect(json_response[:errors][:price]).to include 'is not a number'
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+  
+  describe 'DELETE #destroy' do
+    before do
+      set_api_authorization_header user.auth_token
+      delete :destroy, params: { user_id: user.id, id: product.id }
+    end
+
+    it { should respond_with 204 }
   end
 end
