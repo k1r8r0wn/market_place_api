480eef2feeebec132f721e73d7bcd625da9a6809
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-09-11 20:40:45 +0600

    Add search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,9 @@ class Product < ApplicationRecord
                     presence: true
 
   belongs_to :user
+
+  scope :by_title, ->(keyword) { where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
+  scope :by_above_or_equal_to_price, ->(price) { where('price >= ?', price) }
+  scope :by_below_or_equal_to_price, ->(price) { where('price <= ?', price) }
+  scope :by_recently_updated, -> { order(:updated_at) }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 describe Product, type: :model do
+  let!(:product1) { create(:product, title: 'MacBook', price: 100) }
+  let!(:product2) { create(:product, title: 'iPhone', price: 50) }
+  let!(:product3) { create(:product, title: 'iPad', price: 150) }
+  let!(:product4) { create(:product, title: 'MacBook Pro', price: 99) }
+  
   subject { create(:product) }
-
+  
   describe 'respond' do
     it { should respond_to(:title) }
     it { should respond_to(:price) }
@@ -21,5 +26,41 @@ describe Product, type: :model do
 
   describe 'relation' do
     it { should belong_to :user }
+  end
+
+  describe 'Filter .by_title' do
+    context "when a 'MacBook' title pattern is sent" do
+      it 'returns the 2 products matching' do
+        expect(Product.by_title('MacBook').size).to eq(2)
+      end
+
+      it 'returns the products matching' do
+        expect(Product.by_title('MacBook').sort).to match_array([product1, product4])
+      end
+    end
+  end
+
+  describe 'Filter .by_above_or_equal_to_price' do
+    it 'returns the products which are above or equal to the price' do
+      expect(Product.by_above_or_equal_to_price(100).sort).to match_array([product1, product3])
+    end
+  end
+
+  describe 'Filter .by_below_or_equal_to_price' do
+    it 'returns the products which are below or equal to the price' do
+      expect(Product.by_below_or_equal_to_price(99).sort).to match_array([product2, product4])
+    end
+  end
+
+  describe 'Filter .by_recently_updated' do
+    before(:each) do
+      # touch some products to update them
+      product2.touch
+      product3.touch
+    end
+
+    it 'returns the most updated records' do
+      expect(Product.by_recently_updated).to match_array([product3, product2, product4, product1])
+    end
   end
 end
