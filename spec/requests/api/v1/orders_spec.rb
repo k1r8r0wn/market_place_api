825b024b0a44d4f6634b1f294580e707e551708c
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-10-23 12:26:19 +0600

    Add pagination for orders index action

--- spec/requests/api/v1/orders_spec.rb
+++ spec/requests/api/v1/orders_spec.rb
@@ -21,6 +21,12 @@ describe 'Api V1 Orders', type: :request do
     it "returns a success 200('OK') response" do
       expect(response.status).to eq(200)
     end
+
+    it { expect(json_response).to have_key(:meta) }
+    it { expect(json_response[:meta]).to have_key(:pagination) }
+    it { expect(json_response[:meta][:pagination]).to have_key(:per_page) }
+    it { expect(json_response[:meta][:pagination]).to have_key(:total_pages) }
+    it { expect(json_response[:meta][:pagination]).to have_key(:total_objects) }
   end
 
   describe 'GET #show' do
